3f938f4a46cfb55811cd449913d4958dd570586d
snow <lamboartur94@gmail.com>
2025-04-12 04:20:45 +0200

    docker infrastruture

--- KEYCLOACK/init_db.sql
+++ KEYCLOACK/init_db.sql
@@ -3,7 +3,6 @@
 --
 
 SET default_transaction_read_only = off;
-
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 
@@ -19,13 +18,6 @@ ALTER ROLE postgres WITH SUPERUSER INHERIT CREATEROLE CREATEDB LOGIN REPLICATION
 --
 -- User Configurations
 --
-
-
-
-
-
-
-
 
 --
 -- Databases
@@ -64,10 +56,6 @@ SET row_security = off;
 -- Database "keycloak_db" dump
 --
 
---
--- PostgreSQL database dump
---
-
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.4
 
@@ -88,8 +76,6 @@ SET row_security = off;
 --
 
 CREATE DATABASE keycloak_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
-
-
 ALTER DATABASE keycloak_db OWNER TO postgres;
 
 \connect keycloak_db
@@ -106,15 +92,33 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+-- Création d'un schéma spécifique pour keycloak
+CREATE SCHEMA keycloak_schema AUTHORIZATION keycloak_service;
+
+-- Affectation du schéma par défaut pour keycloak_service :
+ALTER ROLE keycloak_service SET search_path TO keycloak_schema, public;
+
 --
 -- Name: DATABASE keycloak_db; Type: ACL; Schema: -; Owner: postgres
 --
 
 GRANT ALL ON DATABASE keycloak_db TO keycloak_service;
 
+--
+-- Vault Admin Role + Permissions for Password Rotation
+--
 
+-- Création du compte utilisé par Vault
+CREATE ROLE vault_admin;
+ALTER ROLE vault_admin WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'SCRAM-SHA-256$4096:afhiArRIg+IjNUmO0mmDGg==$NqV/Stu1h/vTbP7cJ7h22zw84zLHcvbveGzSsG8GQTY=:TLX4kuKGnwyhztVVy+Ct84x3sQufUvsBGScrMz2ylNY=';
+GRANT CONNECT ON DATABASE keycloak_db TO vault_admin;
 
+-- Donner à vaultadmin le droit de modifier keycloak_service
+GRANT USAGE ON SCHEMA keycloak_schema TO vault_admin;
+ALTER ROLE keycloak_service NOINHERIT;
 
+-- Donner les droits de modification du mot de passe
+GRANT keycloak_service TO vault_admin;
 --
 -- PostgreSQL database dump complete
 --
@@ -150,13 +154,11 @@ SET row_security = off;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
 
-
 --
 -- Name: EXTENSION pgcrypto; Type: COMMENT; Schema: -; Owner: 
 --
 
 COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
-
 
 --
 -- PostgreSQL database dump complete
@@ -165,4 +167,3 @@ COMMENT ON EXTENSION pgcrypto IS 'cryptographic functions';
 --
 -- PostgreSQL database cluster dump complete
 --
-
